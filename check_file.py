f0e32752846f75b754e77b1c9aea10afe0b2286c
Bensuperpc <bensuperpc@gmail.com>
2021-05-26 23:45:24 +0200

    Update python scripts
    Update python scripts
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- check_file.py
+++ check_file.py
@@ -12,9 +12,6 @@ import sys
 import hashlib
 
 import json
-#from itertools import groupby
-
-# or
 
 update = False
 
@@ -26,12 +23,15 @@ if len(sys.argv) >= 2:
     else:
         print('Commande inconnue, please use "-h" or "-update" to display help')
 
+def clean(_file):
+    _file = filter(lambda x: x[0] != 'data.json', _file)
+    _file = filter(lambda x: x[1] != 'null', _file)
+    _file = filter(lambda x: x[0] != os.path.basename(__file__), _file)
+    return _file
 
 def checkfile(fname, mode='sha3_512'):
-    #md5 = hashlib.md5()
-    #sha1 = hashlib.sha1()
-    #h = hashlib.new(mode)
-    h = hashlib.sha3_512()
+    h = hashlib.new(mode)
+    #h = hashlib.sha3_512()
     with open(fname, 'rb') as file:
         block = file.read(1024)
         while block:
@@ -40,7 +40,7 @@ def checkfile(fname, mode='sha3_512'):
     return h.hexdigest()
 
 
-def my_function(_file):
+def file_exist(_file):
     if os.path.exists(_file):
         return (_file, checkfile(_file))
     else:
@@ -60,25 +60,20 @@ for r, d, f in os.walk(path):
 
 pool = ThreadPool(24)
 pool = mp.Pool(processes=24)
-res = pool.map(my_function, files)
+res = pool.map(file_exist, files)
 
 print('Time:', time.process_time() - start)
 
-#res.sort(key=lambda tup: tup[0])
-
-res = filter(lambda x: x[0] != 'data.json', res)
-res = filter(lambda x: x[1] != 'null', res)
-res = filter(lambda x: x[0] != os.path.basename(__file__), res)
+res = clean(res)
 res = list(res)
 
 print(len(res), 'files founds')
 
-params = {}
-params = {'Checker': {"params": {"check": "sha3_512", "multiprocessing": "True"},
-                      'files': [{'file': x, 'check': y} for x, y in res]}}
+params = {'Checker': {"params": {"hash": "sha3_512", "multiprocessing": "True"},
+                      'files': [{'file': x, 'hash': y} for x, y in res]}}
 
 if not os.path.isfile('data.json') or update == True:
-    print('Create or update')
+    print('Create or update: data.json')
     with open('data.json', 'w', encoding='utf-8') as f:
         json.dump(params, f, ensure_ascii=False, sort_keys=True, indent=4)
 
@@ -90,27 +85,28 @@ with open('data.json', 'r') as json_file:
     data = data['Checker']
 
     for p in data['files']:
-        json_data.append((p['file'], p['check']))
+        json_data.append((p['file'], p['hash']))
 
-#res = [i for i in res if not i[0] == os.path.basename(__file__)]
-
-json_data = filter(lambda x: x[0] != 'data.json', json_data)
-json_data = filter(lambda x: x[0] != os.path.basename(__file__), json_data)
+json_data = clean(json_data)
 json_data = list(json_data)
 
 start = time.process_time()
-
-'''
-res_clean = res.copy()
-for i in range(0, len(json_data)):
-    if json_data[i] in res_clean:
-        res_clean.remove(json_data[i])
-'''
 
 res_clean = set(res) - set(json_data)
 json_data_clean = set(json_data) - set(res)
 
 print('Current dir:', len(res_clean), 'Files changed or removed')
 print('In JSON:', len(json_data_clean), 'Files changed or removed')
+
+if (len(res_clean) > 0):
+    print('Update or add file in directory:')
+    for w,e in res_clean:
+        print(f"{w}\t{e}")
+
+if (len(json_data_clean) > 0):
+    print('Update or add file in JSON:')
+    for w,e in json_data_clean:
+        print(f"{w}\t{e}")
+    #print(list(json_data_clean))
 
 print('Time:', time.process_time() - start)
